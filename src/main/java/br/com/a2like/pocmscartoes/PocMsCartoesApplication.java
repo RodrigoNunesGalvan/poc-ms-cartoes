e4da72e548ffd3a63da6cfc779c6dd6c634214ac
RodrigoNunesGalvan <rodrigongalvan@hotmail.com>
2022-09-11 18:42:26 -0300

    Criando microservice de Cartões

---
+++ src/main/java/br/com/a2like/pocmscartoes/PocMsCartoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.a2like.pocmscartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMsCartoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocMsCartoesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/a2like/pocmscartoes/PocMsCartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.a2like.pocmscartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMsCartoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
